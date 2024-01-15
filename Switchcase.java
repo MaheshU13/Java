109b59b387d51c7b51f916310d4bc640e83b42ca
Mahesh Just <153098038+MaheshU13@users.noreply.github.com>
2024-01-15 12:14:57 +0530

    Update Switchcase.java

--- Switchcase.java
+++ Switchcase.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 class Switchcase
 {
-	public static void main(String[ ] arg)
+	public static void main(String args[])
 	{
 	Scanner scin=new Scanner(System.in);
 	System.out.println("Enter a number : ");
@@ -46,7 +46,7 @@ Wednesday
 import java.util.Scanner;
 class Switchcase
 {
-	public static void main(String[ ] arg)
+	public static void main(String args[])
 	{
 	Scanner scin=new Scanner(System.in);
 	System.out.println("Enter a number : ");
